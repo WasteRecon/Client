cd260d931330b88ca6e2bc7c5990f231b8b085df
congthanhptnk <congthanhptnk@gmail.com>
2018-04-23 20:18:18 +0300

    Initial Commit

---
+++ WasteRecon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WasteRecon
+//
+//  Created by Tran Cong Thanh on 23/04/2018.
+//  Copyright © 2018 WasteRecon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
